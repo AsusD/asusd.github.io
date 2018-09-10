938d92c7a4ab49569289dbe2fb21ac8aa363f322
Denis Matveev <mdv.yurist@gmail.com>
2018-09-10 15:21:51 +0300

    Разместил новый проект
    Старый проект - удален, вместо него - размещен новый

--- README.md
+++
@@ -1,1 +0,0 @@
-# asusd.github.io
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,6 @@
 /* ----------------------------------------
-				SCRIPTS
+		INSTALLING YOUR SCRIPTS
 ---------------------------------------- */
-
 
 /* Подключаем прелоадер для сайта через библиотеку "jquery" */
 
@@ -9,45 +8,43 @@ $(window).on('load', function () {
     $preloader = $('.loaderArea'),
       $loader = $preloader.find('.loader');
     $loader.fadeOut();
-    $preloader.delay(350).fadeOut('slow');
+    $preloader.delay(1000).fadeOut('slow');
   });
 
-
-/* ___________________________________________________________________________________________________________________ */
-
-
-/* Связываем скрипт из библиотеки "slick" с нашим слайдером (необходимо вписать имя модификатора - класса) */
-
-$('.slider').slick
-(
-{
-	dots: true,
-	autoplay: true,
-	autoplaySpeed: 4500,
-	speed: 1200,
-	slidesToShow: 1
-}
-);
-
-
-/* ___________________________________________________________________________________________________________________ */
-
-
 /* Связываем скрипт из библиотеки "jquery" для плавного перехода по ссылкам навигации внутри сайта
-(для этого всем ссылкам из навигации необходимо так же присвоить отдельный класс "scrollto") */
+(для этого всем ссылкам из навигации необходимо так же присвоить класс "scrollto") */
 
 $(document).ready(function() {
 $("a.scrollto").click(function () {
 var elementClick = $(this).attr("href")
 var destination = $(elementClick).offset().top;
-jQuery("html:not(:animated),body:not(:animated)").animate({scrollTop: destination}, 800);
+jQuery("html:not(:animated),body:not(:animated)").animate({scrollTop: destination}, 1200);
 return false;
 });
 });
 
+/* Прописываем скрипт для кнопки меню */
 
-/* ___________________________________________________________________________________________________________________ */
+$('.menu-btn').on('click', function(e) {
+  e.preventDefault();
+  $(this).toggleClass('menu-btn_active');
+  $('.menu-nav').toggleClass('menu-nav_active');
+});
 
+/* Связываем скрипт из библиотеки "jquery" для кнопки "НАВЕРХ" ("ToTOP") */
+
+$(function() {
+$(window).scroll(function() {
+if($(this).scrollTop() != 0) {
+$('#toTop').fadeIn();
+} else {
+$('#toTop').fadeOut();
+}
+});
+$('#toTop').click(function() {
+$('body,html').animate({scrollTop:0},1600);
+});
+});
 
 /* Подключаем плавную прокрутку страниц из библиотеки "jquery" */
 
@@ -58,37 +55,3 @@ $(function() {
 	// Custom Easing
 	jQuery.scrollSpeed(100, 800, 'easeOutCubic');
 });
-
-
-/* ___________________________________________________________________________________________________________________ */
-
-
-/* Прописываем скрипт для кнопки меню */
-
-$('.menu-btn').on('click', function(e) {
-	e.preventDefault();
-	$(this).toggleClass('menu-btn_active');
-	$('.menu-nav').toggleClass('menu-nav_active');
-  });
-
-
-/* ___________________________________________________________________________________________________________________ */
-
-
-/* Связываем скрипт из библиотеки "jquery" для кнопки "НАВЕРХ" ("ToTOP") */
-
-$(function() {
-	$(window).scroll(function() {
-	if($(this).scrollTop() != 0) {
-	$('#toTop').fadeIn();
-	} else {
-	$('#toTop').fadeOut();
-	}
-	});
-	$('#toTop').click(function() {
-	$('body,html').animate({scrollTop:0},1600);
-	});
-	});
-
-
-/* ___________________________________________________________________________________________________________________ */
